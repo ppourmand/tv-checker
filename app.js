67914fecf288cc1957f1264ce4e05d405faab450
Pasha Pourmand <pasha.pourmand@gmail.com>
2017-06-14 21:19:31 -0700

    init

---
+++ app.js
@@ -0,0 +1,6 @@
+var searchButton = document.getElementById('search-btn');
+
+searchButton.onclick = function(){
+  var searchValue = document.getElementById('television-name-search').value;
+  console.log(searchValue);
+}
